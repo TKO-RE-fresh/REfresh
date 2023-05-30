ca2891c2cddb905c39a0db3d13c987384d1f52a3
jiokim-7 <now20412041@gmail.com>
2023-05-30 19:19:01 +0900

    fix: 스프링 시큐리티 임시해제

---
+++ backend/src/main/java/tko/refresh/util/jwt/JwtAuthMember.java
@@ -0,0 +1,26 @@
+package tko.refresh.util.jwt;
+
+
+import java.util.Optional;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import tko.refresh.domain.entity.Member;
+
+public class JwtAuthMember {
+    public Optional<Member> getJwtAuthMember() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+            if(principal instanceof UserDetails) {
+                MemberDetailsImpl memberDetails = (MemberDetailsImpl) principal;
+                return Optional.of(memberDetails.getMember());
+            }
+        }
+        return Optional.empty();
+    }
+
+
+}
