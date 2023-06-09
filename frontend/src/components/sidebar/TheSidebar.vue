6a6b92f89d7d52162578458d42da5726b4272e9c
JuHee Park <55836020+juhee99@users.noreply.github.com>
2023-06-09 15:58:45 +0900

    fix: 마이페이지로그인한 유저 정보로 수정

--- frontend/src/components/sidebar/TheSidebar.vue
+++ frontend/src/components/sidebar/TheSidebar.vue
@@ -70,7 +70,7 @@
               <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
               <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
             </svg>
-            <span class="ml-3">연차 신청</span>
+            <span class="flex-1 ml-3 whitespace-nowrap">연차 신청</span>
           </router-link>
         </li>
         <li>
