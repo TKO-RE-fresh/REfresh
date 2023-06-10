850fca7f290b5ee158234edc20b9dc8120ea855d
mowgood <woforky@gmail.com>
2023-06-11 03:04:11 +0900

    fix: 관리자 모달 버튼 UI 수정

--- frontend/src/views/admin/AnnualStatusModal.vue
+++ frontend/src/views/admin/AnnualStatusModal.vue
@@ -59,7 +59,7 @@
 
             <div class="p-3 mt-2 text-center space-x-4 md:block font-mice">
               <button
-                class="mb-2 md:mb-0 bg-[#003566] px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-md hover:shadow-lg hover:bg-[#f6aa1c]"
+                class="mb-2 md:mb-0 bg-[#003566] px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-white rounded-md hover:shadow-lg hover:bg-[#f6aa1c]"
                 @click="accetpHandler"
               >
                 확인
